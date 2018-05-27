741da18602ba81c8e8f62b5cd0167ec02022e7ad
Omar Ozgur <oozgur217@gmail.com>
2018-05-27 01:17:38 -0700

    Add lesion masking logic

--- thresholding/main.py
+++ thresholding/main.py
@@ -57,6 +57,47 @@ def plot_images(slices, rows=4, cols=6, start=0, freq=1):
     # Display the plot
     plt.show()
 
+# Create a lesion mask based on image processing techniques
+def create_lesion_mask(image):
+
+    # Save image details
+    original = image.astype(np.float)
+    rows, cols = image.shape[0], image.shape[1]
+    
+    # Use the mean and standard deviation to accentuate differences
+    mean = np.mean(image)
+    std = np.std(image)
+    image = image - mean
+    image = image / std
+
+    # Find the average, max, and min
+    middle = image[int(cols * 0.25):int(cols * 0.75), int(rows * 0.25):int(rows * 0.75)] 
+    mean = np.mean(image)  
+    max = np.max(image)
+    min = np.min(image)
+
+    # Use the KMeans clustering algorithm to find groups of pixels
+    kmeans = KMeans(n_clusters=5).fit(np.reshape(middle, [np.prod(middle.shape), 1]))
+    centers = sorted(kmeans.cluster_centers_.flatten())
+    threshold = centers[3]
+
+    # Use inertia to determine how close pixels are to their cluster centers
+    inertia = kmeans.inertia_
+    if inertia > 750:
+        original = exposure.rescale_intensity(original, out_range=(0.0, 1.0)) * 0.3
+        original[0][0] = 1.0
+        return original
+
+    # Use the threshold to filter possible lesions
+    thresh_img = np.where(image > threshold, 1.0, 0.3)
+
+    # Erode and then dilate regions to remove small anomalies
+    eroded = morphology.erosion(thresh_img, np.ones([10, 10]))
+    dilation = morphology.dilation(eroded, np.ones([10, 10]))
+
+    # Apply the dilated regions to the original image
+    return dilation * original
+
 # Load dicom image data
 def load_data(saved=False, resize=False):
     if saved and images_name not in os.listdir(output_path):
@@ -72,10 +113,17 @@ def load_data(saved=False, resize=False):
     return images
 
 # Plot a set of dicom images
-def plot():
+def plot(masked=False):
 
     # Load the saved image
     images = np.load(images_path)
+
+    # Create the lesion mask
+    if masked:
+        masked_lesion = []
+        for img in images:
+            masked_lesion.append(create_lesion_mask(img))
+        images = masked_lesion
 
     # Plot the results
     rows, cols = 4, 6
@@ -86,4 +134,4 @@ def plot():
 load_data()
 
 # Plot the images with lesion masks
-plot()
+plot(masked=True)
